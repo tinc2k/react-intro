d6becb88ed43f680f91b3e6adb812af7d6f18382
Tin Crnkovic <tinc2k@gmail.com>
2015-04-27 00:01:01 +0200

    a reactionary tale

---
+++ projects/Newsfeed/Newsfeed/App_Start/BundleConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.Optimization.React;
+
+namespace Newsfeed.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/bootstrap.css",
+            //          "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/core").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/react/react-with-addons-{version}.js",
+                        "~/Scripts/react/JSXTransformer-{version}.js"
+                        ));
+
+            bundles.Add(new JsxBundle("~/bundles/components").Include(
+                "~/Scripts/HelloWorld.jsx"
+            ));
+
+            //BundleTable.EnableOptimizations = true;
+        }
+
+    }
+}
---
+++ projects/Newsfeed/Newsfeed/Scripts/HelloWorld.jsx
@@ -0,0 +1,24 @@
+﻿var HelloWorld = React.createClass({
+    render: function() {
+        return (
+			<div className="card">
+				<h2>Hello {this.props.name}!</h2>
+				<div>This component is alive for {this.state.time} seconds.</div>
+			</div>
+		);
+	},
+	getInitialState: function() {
+      return { time: 0 };
+    },
+	 
+	componentDidMount: function() {
+		this._timeout = setInterval(function () {
+            this.setState({time: this.state.time + 1});
+         }.bind(this), 1000);
+
+	},
+	componentWillUnmount: function(){
+		// Clear the timeout
+		clearInterval(this._timeout); 
+    }
+});
